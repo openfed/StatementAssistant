58dc3a677a01273b579c38c4479eaca240f9cc77
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2021-03-04 14:06:30 +0100

    0001-4 Refactor/move InfoBox on homepage

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,28 +1,10 @@
 import React, { useState } from "react";
 import { Modal as M } from "react-responsive-modal";
 import { Trans } from "@lingui/macro";
+import CloseIcon from "./icons/close";
 
 export default function Modal({ children }) {
   const [open, setOpen] = useState(false);
-
-  const closeIcon = (
-    <svg
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fas"
-      data-icon="times"
-      className="svg-inline--fa fa-times fa-w-11"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 352 512"
-      width={22} height={22}
-    >
-      <path
-        fill="#ffffff"
-        d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-      ></path>
-    </svg>
-  );
 
   return (
     <div className="db">
@@ -41,7 +23,7 @@ export default function Modal({ children }) {
         open={open}
         onClose={() => setOpen(false)}
         center
-        closeIcon={closeIcon}
+        closeIcon={CloseIcon}
         ariaLabelledby="modal-title"
       >
         <h2 id="modal-title"><Trans>Help</Trans></h2>
