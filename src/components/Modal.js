640a4f20d59f5c7300a8f647c00766bd2391527c
Valentijn Steenhoudt <vsteenhoudt@prophets.be>
2021-03-09 10:25:24 +0100

    add scroll modal into view

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Modal as M } from "react-responsive-modal";
 import { Trans } from "@lingui/macro";
 import CloseIcon from "./icons/close";
 
 export default function Modal({ children }) {
   const [open, setOpen] = useState(false);
+  const [buttonPos, setButtonPos] = useState(0);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (modalRef.current && open) {
+        const { top } = modalRef.current.getBoundingClientRect();
+        window.scrollTo(0, top - 100);
+      }
+    }, 100);
+  }, [open]);
 
   return (
     <div className="db">
@@ -15,18 +26,22 @@ export default function Modal({ children }) {
         onClick={(e) => {
           e.preventDefault();
           setOpen(true);
+          setButtonPos(e.currentTarget.getBoundingClientRect().top);
         }}
       >
         <Trans>Get help</Trans>
       </a>
       <M
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={() => {
+          setOpen(false);
+          window.scrollTo(0, buttonPos - 100);
+        }}
         center
         closeIcon={CloseIcon}
         ariaLabelledby="modal-title"
       >
-        <h2 id="modal-title"><Trans>Help</Trans></h2>
+        <h2 id="modal-title" ref={ modalRef }><Trans>Help</Trans></h2>
         { children }
       </M>
     </div>
